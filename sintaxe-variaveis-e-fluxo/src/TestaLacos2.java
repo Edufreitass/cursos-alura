5fa9ce48237938b057dd4020dc13bf12fdd173b0
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-07-28 23:06:21 -0300

    Exercitando laços aninhados e break

--- sintaxe-variaveis-e-fluxo/src/TestaLacos2.java
+++ sintaxe-variaveis-e-fluxo/src/TestaLacos2.java
@@ -12,6 +12,18 @@ public class TestaLacos2 {
             System.out.println();
         }
 
+        System.out.println();
+
+        for (int linha = 0; linha < 5; linha++) {
+            for (int coluna = 0; coluna < 5; coluna++) {
+                if (coluna > linha) {
+                    break;
+                }
+                System.out.print(coluna + 1);
+            }
+            System.out.println();
+        }
+
     }
 
 }
