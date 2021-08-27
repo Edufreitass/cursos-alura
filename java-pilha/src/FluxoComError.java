ffb92fe1f18c2e852a7e578f28dc58f719e1e12a
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-08-27 00:15:14 -0300

    Checked e unchecked

--- java-pilha/src/FluxoComError.java
+++ java-pilha/src/FluxoComError.java
@@ -1,10 +1,10 @@
-public class Fluxo {
+public class FluxoComError {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
         try {
             metodo1();
-        } catch (ArithmeticException | NullPointerException | MinhaExcecao e) {
+        } catch (ArithmeticException | NullPointerException e) {
             String msg = e.getMessage();
             System.out.println("Exception " + msg);
             e.printStackTrace();
@@ -12,18 +12,16 @@ public class Fluxo {
         System.out.println("Fim do main");
     }
 
-    private static void metodo1() throws MinhaExcecao {
+    private static void metodo1() {
         System.out.println("Ini do metodo1");
         metodo2();
         System.out.println("Fim do metodo1");
     }
 
-    private static void metodo2() throws MinhaExcecao {
-        System.out.println("Ini do metodo2");
-
-        throw new MinhaExcecao("deu muito errado");
-
-//        System.out.println("Fim do metodo2");
+    private static void metodo2() {
+        System.out.println("ini do método 2");
+        metodo2();
+        System.out.println("fim do metodo 2");
     }
 
 }
