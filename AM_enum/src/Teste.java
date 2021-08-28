2a4c9bf02eabe9149c83f716d3879ff0a33b2add
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-08-28 01:25:53 -0300

    alura+ Enums

---
+++ AM_enum/src/Teste.java
@@ -0,0 +1,19 @@
+public class Teste {
+
+    public static void main(String[] args) {
+
+        Prioridade pMin = Prioridade.MIN;
+        Prioridade pMax = Prioridade.MAX;
+
+        System.out.println(pMin.name()); // MIN
+        System.out.println(pMax.name()); // MAX
+
+        System.out.println(pMin.ordinal()); // 0
+        System.out.println(pMax.ordinal()); // 2
+
+        System.out.println(pMin.getValor()); // 1
+        System.out.println(pMax.getValor()); // 10
+
+    }
+
+}
