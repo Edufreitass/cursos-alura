83a54ab21a7047f5ae17ffee95f5f64737106b37
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-09-02 23:46:00 -0300

    O método toString()

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/Teste.java
@@ -8,9 +8,9 @@ public class Teste {
 
     public static void main(String[] args) {
 
-        System.out.println("x");
-        System.out.println(3);
-        System.out.println(false);
+//        System.out.println("x");
+//        System.out.println(3);
+//        System.out.println(false);
 
         ContaCorrente cc = new ContaCorrente(22, 33);
         ContaPoupanca cp = new ContaPoupanca(33, 22);
