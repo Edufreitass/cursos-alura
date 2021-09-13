24775b1e3ea213ef05c0b316077c8be797890091
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-09-12 22:42:19 -0300

    Outras entradas e saídas

--- java-io/src/br/com/alura/java/io/teste/TesteCopiarArquivo.java
+++ java-io/src/br/com/alura/java/io/teste/TesteCopiarArquivo.java
@@ -1,24 +1,27 @@
 package br.com.alura.java.io.teste;
 
 import java.io.*;
+import java.net.Socket;
 
 public class TesteCopiarArquivo {
 
     public static void main(String[] args) throws IOException {
 
-        InputStream fis = new FileInputStream("lorem.txt");
+        Socket socket = new Socket();
+        InputStream fis = socket.getInputStream(); // System.in; // new FileInputStream("lorem.txt");
         Reader isr = new InputStreamReader(fis);
         BufferedReader br = new BufferedReader(isr);
 
-        OutputStream fos = new FileOutputStream("lorem2.txt");
+        OutputStream fos = socket.getOutputStream(); // System.out; // new FileOutputStream("lorem2.txt");
         Writer osw = new OutputStreamWriter(fos);
         BufferedWriter bw = new BufferedWriter(osw);
 
         String linha = br.readLine();
 
-        while (linha != null) {
+        while (linha != null && !linha.isEmpty()) {
             bw.write(linha);
             bw.newLine();
+//            bw.flush();
             linha = br.readLine();
         }
 
