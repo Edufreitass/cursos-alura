337a2433f053c1984180a673adc41aa6a857e4ce
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-09-14 23:04:35 -0300

    PrintWriter e PrintStream

---
+++ java-io/src/br/com/alura/java/io/teste/TesteEscrita3.java
@@ -0,0 +1,26 @@
+package br.com.alura.java.io.teste;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class TesteEscrita3 {
+
+    public static void main(String[] args) throws IOException {
+
+        // Fluxo de Saída com Arquivo
+//        OutputStream fos = new FileOutputStream("lorem2.txt");
+//        Writer osw = new OutputStreamWriter(fos);
+//        BufferedWriter bw = new BufferedWriter(osw);
+
+//        BufferedWriter bw = new BufferedWriter(new FileWriter("lorem2.txt"));
+
+//        PrintStream ps = new PrintStream("lorem2.txt");
+        PrintWriter ps = new PrintWriter("lorem2.txt");
+
+        ps.println("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
+        ps.println();
+        ps.println("ahdsoauhsdoahdoashd ads sdas dad asdadssadfssa sf sf sa sdsfdsfsd ass a");
+
+        ps.close();
+    }
+}
