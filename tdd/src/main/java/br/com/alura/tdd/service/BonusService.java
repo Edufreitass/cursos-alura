a3828f4d5d5faadc472d7f4553b0166216985187
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-10-02 11:09:14 -0300

    Testando uma funcionalidade

--- tdd/src/main/java/br/com/alura/tdd/service/BonusService.java
+++ tdd/src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -3,6 +3,7 @@ package br.com.alura.tdd.service;
 import br.com.alura.tdd.modelo.Funcionario;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class BonusService {
 
@@ -11,6 +12,6 @@ public class BonusService {
         if (valor.compareTo(new BigDecimal("1000")) > 0) {
             valor = BigDecimal.ZERO;
         }
-        return valor;
+        return valor.setScale(2, RoundingMode.HALF_UP);
     }
 }
