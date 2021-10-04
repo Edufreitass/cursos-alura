b840935349668ebdf8fbf4b93c0b6c05aecdfb35
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-10-04 20:42:57 -0300

    Prioridades

--- banheiro/src/br/com/alura/threads/Principal.java
+++ banheiro/src/br/com/alura/threads/Principal.java
@@ -8,6 +8,7 @@ public class Principal {
         Thread convidado1 = new Thread(new TarefaNumero1(banheiro), "Joao");
         Thread convidado2 = new Thread(new TarefaNumero2(banheiro), "Pedro");
         Thread limpeza = new Thread(new TarefaLimpeza(banheiro), "Limpeza");
+        limpeza.setPriority(10);
         limpeza.setDaemon(true);
 //        Thread convidado3 = new Thread(new TarefaNumero1(banheiro), "Maria");
 //        Thread convidado4 = new Thread(new TarefaNumero2(banheiro), "Ana");
