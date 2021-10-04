52bda9ec5b08995109d42a46e21af346aa18a5bd
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-10-03 22:37:01 -0300

    Threads manipulando listas

---
+++ threads-1/src/br/com/alura/lista/Principal.java
@@ -0,0 +1,18 @@
+package br.com.alura.lista;
+
+public class Principal {
+
+    public static void main(String[] args) throws InterruptedException {
+        Lista lista = new Lista();
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(new TarefaAdicionarElemento(lista, i)).start();
+        }
+
+        Thread.sleep(200);
+
+        for (int i = 0; i < lista.tamanho(); i++) {
+            System.out.println(i + " - " + lista.pegaElemento(i));
+        }
+    }
+}
