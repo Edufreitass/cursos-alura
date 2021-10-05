5425533e89b6a677f2c1f8517d82a5e54f4f7784
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-10-04 21:54:05 -0300

    Comentários e conceitos

--- threads-1/src/br/com/alura/banco/TarefaAcessaBancoProcedimento.java
+++ threads-1/src/br/com/alura/banco/TarefaAcessaBancoProcedimento.java
@@ -12,6 +12,7 @@ public class TarefaAcessaBancoProcedimento implements Runnable {
 
     @Override
     public void run() {
+        // Podemos usar um bloco synchronized dentro do outro.
         synchronized (pool) {
             System.out.println("peguei a conexao");
             pool.getConnection();
@@ -23,7 +24,10 @@ public class TarefaAcessaBancoProcedimento implements Runnable {
         }
     }
 
-    // DEADLOCK
+    /**
+     * DEADLOCK
+     * Podemos evitar o Deadlock obtendo as chaves dos objetos na mesma ordem, em todos os threads.
+     */
 //    @Override
 //    public void run() {
 //        synchronized (transacao) {
