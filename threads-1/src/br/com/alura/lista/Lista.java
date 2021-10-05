551fad3bca40bf1abcf1b9415dd9815f7a71010e
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-10-04 21:01:52 -0300

    Revisitando a nossa lista

--- threads-1/src/br/com/alura/lista/Lista.java
+++ threads-1/src/br/com/alura/lista/Lista.java
@@ -8,6 +8,17 @@ public class Lista {
     public synchronized void adicionaElementos(String elemento) {
         this.elementos[indice] = elemento;
         this.indice++;
+
+        try {
+            Thread.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if (this.indice == this.elementos.length) {
+            System.out.println("lista tá cheia, notificando");
+            this.notify();
+        }
     }
 
     public int tamanho() {
