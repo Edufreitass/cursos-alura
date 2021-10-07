5e5c71fffba2efad6d9113083d2c6e1433ee3280
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-10-06 22:39:16 -0300

    Enviando o primeiro comando para o nosso servidor

--- curso-threads-2/servidor-tarefas/src/br/com/alura/cliente/ClienteTarefas.java
+++ curso-threads-2/servidor-tarefas/src/br/com/alura/cliente/ClienteTarefas.java
@@ -1,5 +1,6 @@
 package br.com.alura.cliente;
 
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -9,9 +10,14 @@ public class ClienteTarefas {
         Socket socket = new Socket("localhost", 12345);
         System.out.println("conexao estabelecida");
 
+        PrintStream saida = new PrintStream(socket.getOutputStream());
+        saida.println("c1");
+
         Scanner teclado = new Scanner(System.in);
         teclado.nextLine();
 
+        saida.close();
+        teclado.close();
         socket.close();
     }
 }
