1d93c23931fba5e3996b9de35be8c32774e917b0
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-10-07 23:49:36 -0300

    Melhorando o cliente

--- curso-threads-2/servidor-tarefas/src/br/com/alura/cliente/ClienteTarefas.java
+++ curso-threads-2/servidor-tarefas/src/br/com/alura/cliente/ClienteTarefas.java
@@ -1,5 +1,6 @@
 package br.com.alura.cliente;
 
+import java.io.IOException;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
@@ -10,14 +11,48 @@ public class ClienteTarefas {
         Socket socket = new Socket("localhost", 12345);
         System.out.println("conexao estabelecida");
 
-        PrintStream saida = new PrintStream(socket.getOutputStream());
-        saida.println("c1");
+        Thread threadEnviaComando = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("Pode enviar comandos!");
+                    PrintStream saida = new PrintStream(socket.getOutputStream());
+                    Scanner teclado = new Scanner(System.in);
+                    while (teclado.hasNextLine()) {
+                        String linha = teclado.nextLine();
+                        if (linha.trim().equals("")) {
+                            break;
+                        }
+                        saida.println(linha);
+                    }
+                    saida.close();
+                    teclado.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
 
-        Scanner teclado = new Scanner(System.in);
-        teclado.nextLine();
+        Thread threadRecebeResposta = new Thread(() -> {
+            try {
+                System.out.println("recebendo dados do servidor");
+                Scanner respostaServidor = new Scanner(socket.getInputStream());
+                while (respostaServidor.hasNextLine()) {
+                    String linha = respostaServidor.nextLine();
+                    System.out.println(linha);
+                }
+                respostaServidor.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
 
-        saida.close();
-        teclado.close();
+        threadRecebeResposta.start();
+        threadEnviaComando.start();
+
+        threadEnviaComando.join();
+
+        System.out.println("Fechando socket do cliente");
         socket.close();
     }
 }
