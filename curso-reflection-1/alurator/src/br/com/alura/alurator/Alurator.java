a66c2049dbe6b587fe7cbf7352c2149123307c0d
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-10-11 19:59:08 -0300

    Tratamento especial para InvocationTargetException

--- curso-reflection-1/alurator/src/br/com/alura/alurator/Alurator.java
+++ curso-reflection-1/alurator/src/br/com/alura/alurator/Alurator.java
@@ -23,6 +23,10 @@ public class Alurator {
                 .refleteClasse(pacoteBase + nomeControle)
                 .criaInstancia()
                 .getMetodo(nomeMetodo, params)
+                .comTratamentoDeExcecao((metodo, e) -> {
+                    System.out.println("Erro no método " + metodo.getName() + " da classe " + metodo.getDeclaringClass().getName() + ".\n\n");
+                    throw new RuntimeException("ERRO!");
+                })
                 .invoca();
 
         System.out.println(retorno);
--- curso-reflection-1/alurator/src/br/com/alura/alurator/reflexao/ManipuladorMetodo.java
+++ curso-reflection-1/alurator/src/br/com/alura/alurator/reflexao/ManipuladorMetodo.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.stream.Stream;
 
 public class ManipuladorMetodo {
@@ -12,11 +13,17 @@ public class ManipuladorMetodo {
     private Object instancia;
     private Method metodo;
     private Map<String, Object> params;
+    private BiFunction<Method, InvocationTargetException, Object> tratamentoExcecao;
 
     public ManipuladorMetodo(Object instancia, Method metodo, Map<String, Object> params) {
         this.instancia = instancia;
         this.metodo = metodo;
         this.params = params;
+    }
+
+    public ManipuladorMetodo comTratamentoDeExcecao(BiFunction<Method, InvocationTargetException, Object> tratamentoExcecao) {
+        this.tratamentoExcecao = tratamentoExcecao;
+        return this;
     }
 
     public Object invoca() {
@@ -30,6 +37,9 @@ public class ManipuladorMetodo {
             e.printStackTrace();
             throw new RuntimeException(e);
         } catch (InvocationTargetException e) {
+            if (tratamentoExcecao != null) {
+                return tratamentoExcecao.apply(metodo, e);
+            }
             e.printStackTrace();
             throw new RuntimeException("Erro dentro do metodo!", e);
         }
