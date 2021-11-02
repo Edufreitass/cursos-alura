8c85a23006189200bf7c917866a7d1d1c6a5c0ab
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-11-02 15:03:30 -0300

    Calculando descontos

--- arquitetura-e-design-de-projetos/design-patterns-I/loja/src/br/com/alura/loja/TestesImpostos.java
+++ arquitetura-e-design-de-projetos/design-patterns-I/loja/src/br/com/alura/loja/TestesImpostos.java
@@ -1,7 +1,7 @@
 package br.com.alura.loja;
 
 import br.com.alura.loja.imposto.CalculadoraDeImpostos;
-import br.com.alura.loja.imposto.TipoImposto;
+import br.com.alura.loja.imposto.ISS;
 import br.com.alura.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
@@ -9,8 +9,8 @@ import java.math.BigDecimal;
 public class TestesImpostos {
 
     public static void main(String[] args) {
-        Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+        Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
         CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
-        System.out.println(calculadora.calcular(orcamento, TipoImposto.ISS));
+        System.out.println(calculadora.calcular(orcamento, new ISS()));
     }
 }
