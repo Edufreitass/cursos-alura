4465ef8a07ae3864dbd2e4cd3882308083cbbd16
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-11-06 13:09:30 -0300

    Lentidão no cálculo do orçamento

--- arquitetura-e-design-de-projetos/design-patterns-II/loja/src/br/com/alura/loja/TestesComposicao.java
+++ arquitetura-e-design-de-projetos/design-patterns-II/loja/src/br/com/alura/loja/TestesComposicao.java
@@ -10,12 +10,12 @@ public class TestesComposicao {
     public static void main(String[] args) {
         Orcamento antigo = new Orcamento();
         antigo.adicionarItem(new ItemOrcamento(new BigDecimal("200")));
-        antigo.reprovar();
 
         Orcamento novo = new Orcamento();
         novo.adicionarItem(new ItemOrcamento(new BigDecimal("500")));
         novo.adicionarItem(antigo);
 
+        System.out.println(novo.getValor());
         System.out.println(novo.getValor());
     }
 }
