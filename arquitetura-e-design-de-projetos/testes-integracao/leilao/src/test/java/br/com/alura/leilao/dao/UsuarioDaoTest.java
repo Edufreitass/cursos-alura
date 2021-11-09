fb319e9444f5ab74f04735a3a83387da964f7ed0
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-11-08 21:22:00 -0300

    Adaptando as classes para os testes

--- arquitetura-e-design-de-projetos/testes-integracao/leilao/src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ arquitetura-e-design-de-projetos/testes-integracao/leilao/src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -3,15 +3,18 @@ package br.com.alura.leilao.dao;
 import br.com.alura.leilao.model.Usuario;
 import org.junit.jupiter.api.Test;
 
+import javax.persistence.EntityManager;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class UsuarioDaoTest {
 
     private UsuarioDao dao;
+    private EntityManager em;
 
     @Test
     void testeBuscaDeUsuarioPeloUsername() {
-        this.dao = new UsuarioDao();
+        this.dao = new UsuarioDao(em);
         Usuario usuario = dao.buscarPorUsername("fulano");
         assertNotNull(usuario);
     }
