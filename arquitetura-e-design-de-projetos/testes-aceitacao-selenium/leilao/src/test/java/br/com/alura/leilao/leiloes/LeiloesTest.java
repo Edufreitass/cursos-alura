d2d36a2a148652b0a32f15ee19675a0bbc548151
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-11-13 12:45:35 -0300

    Testando o cadastro de leilão

--- arquitetura-e-design-de-projetos/testes-aceitacao-selenium/leilao/src/test/java/br/com/alura/leilao/leiloes/LeiloesTest.java
+++ arquitetura-e-design-de-projetos/testes-aceitacao-selenium/leilao/src/test/java/br/com/alura/leilao/leiloes/LeiloesTest.java
@@ -2,7 +2,11 @@ package br.com.alura.leilao.leiloes;
 
 import br.com.alura.leilao.login.LoginPage;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class LeiloesTest {
 
@@ -19,5 +23,12 @@ public class LeiloesTest {
         paginaDeLogin.preencheFormularioDeLogin("fulano", "pass");
         this.paginaDeLeiloes = paginaDeLogin.efetuaLogin();
         CadastroLeilaoPage paginaDeCadastro = paginaDeLeiloes.carregarFormulario();
+
+        String hoje = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        String nome = "Leilao do dia " + hoje;
+        String valor = "500.00";
+
+        this.paginaDeLeiloes = paginaDeCadastro.cadastrarLeilao(nome, valor, hoje);
+        Assertions.assertTrue(paginaDeLeiloes.isLeilaoCadastrado(nome, valor, hoje));
     }
 }
