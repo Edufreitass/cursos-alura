070596c74d8f5cb511ab6f21f593cd6b70eb408e
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-11-17 22:32:50 -0300

    Implementando Steps

---
+++ arquitetura-e-design-de-projetos/bdd-cucumber/leilao/src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLancesSteps.java
@@ -0,0 +1,38 @@
+package br.com.alura.leilao.acceptance.steps;
+
+import br.com.alura.leilao.model.Lance;
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Usuario;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+import org.junit.jupiter.api.Assertions;
+
+import java.math.BigDecimal;
+
+public class PropondoLancesSteps {
+
+    private Lance lance;
+    private Leilao leilao;
+
+    @Dado("um lance valido")
+    public void dado_um_lance_valido() {
+        Usuario usuario = new Usuario("fulano");
+        lance = new Lance(usuario, BigDecimal.TEN);
+    }
+
+    @Quando("propoe o lance")
+    public void quando_propoe_o_lance() {
+        leilao = new Leilao("Tablet XPTO");
+        leilao.propoe(lance);
+    }
+
+    @Entao("o lance eh aceito")
+    public void entao_o_lance_eh_aceito() {
+        Assertions.assertEquals(1, leilao.getLances().size());
+        Assertions.assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
+    }
+}
