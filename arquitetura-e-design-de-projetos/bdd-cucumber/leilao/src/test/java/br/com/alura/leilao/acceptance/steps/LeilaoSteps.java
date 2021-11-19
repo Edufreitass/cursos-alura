ba740e7b0d5d83b22ae5d32e0feefff29d0ee8b4
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-11-19 19:10:50 -0300

    Tags e Novo Leilão

---
+++ arquitetura-e-design-de-projetos/bdd-cucumber/leilao/src/test/java/br/com/alura/leilao/acceptance/steps/LeilaoSteps.java
@@ -0,0 +1,33 @@
+package br.com.alura.leilao.acceptance.steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class LeilaoSteps {
+
+    @Dado("um usuario logado")
+    public void um_usuario_logado() {
+
+    }
+
+    @Quando("acessa a pagina de um novo leilao")
+    public void acessa_a_pagina_de_um_novo_leilao() {
+
+    }
+
+    @Quando("preenche o formulario com dados validos")
+    public void preenche_o_formulario_com_dados_validos() {
+
+    }
+
+    @Entao("volta para a pagina de leiloes")
+    public void volta_para_a_pagina_de_leiloes() {
+
+    }
+
+    @Entao("o novo leilao aparece na tabela")
+    public void o_novo_leilao_aparece_na_tabela() {
+
+    }
+}
