4e3a704e5e124e0326efd4009e907102b8da5560
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-11-18 23:12:34 -0300

    Trabalhando com DataTable

--- arquitetura-e-design-de-projetos/bdd-cucumber/leilao/src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLancesSteps.java
+++ arquitetura-e-design-de-projetos/bdd-cucumber/leilao/src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLancesSteps.java
@@ -3,6 +3,7 @@ package br.com.alura.leilao.acceptance.steps;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.model.Usuario;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.pt.Dado;
@@ -13,6 +14,7 @@ import org.junit.jupiter.api.Assertions;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class PropondoLancesSteps {
 
@@ -68,12 +70,30 @@ public class PropondoLancesSteps {
 
     @Dado("um lance invalido de {double} reais e do usuario {string}")
     public void um_lance_invalido_de_reais(Double valor, String nomeUsuario) {
-        System.out.println(nomeUsuario);
+//        System.out.println(nomeUsuario);
         this.lance = new Lance(new BigDecimal(valor));
     }
 
     @Entao("o lance nao eh aceito")
     public void o_lance_nao_eh_aceito() {
         Assertions.assertEquals(0, leilao.getLances().size());
+    }
+
+    @Dado("dois lances")
+    public void dois_lances(DataTable dataTable) {
+        List<Map<String, String>> valores = dataTable.asMaps();
+        for (Map<String, String> mapa : valores) {
+            String valor = mapa.get("valor");
+            String nome = mapa.get("nomeUsuario");
+
+            Lance lance = new Lance(new Usuario(nome), new BigDecimal(valor));
+            lista.add(lance);
+        }
+    }
+
+    @Entao("o segundo lance nao eh aceito")
+    public void o_segundo_lance_nao_eh_aceito() {
+        Assertions.assertEquals(1, leilao.getLances().size());
+        Assertions.assertEquals(this.lista.get(0).getValor(), leilao.getLances().get(0).getValor());
     }
 }
