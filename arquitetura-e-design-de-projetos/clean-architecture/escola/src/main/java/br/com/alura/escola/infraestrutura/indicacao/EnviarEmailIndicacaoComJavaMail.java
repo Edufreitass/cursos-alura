7a080e26d94af7c48fb6eab59810f87a10945179
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-11-22 20:18:00 -0300

    E-mail de indicação

---
+++ arquitetura-e-design-de-projetos/clean-architecture/escola/src/main/java/br/com/alura/escola/infraestrutura/indicacao/EnviarEmailIndicacaoComJavaMail.java
@@ -0,0 +1,12 @@
+package br.com.alura.escola.infraestrutura.indicacao;
+
+import br.com.alura.escola.aplicacao.indicacao.EnviarEmailIndicacao;
+import br.com.alura.escola.dominio.aluno.Aluno;
+
+public class EnviarEmailIndicacaoComJavaMail implements EnviarEmailIndicacao {
+
+    @Override
+    public void enviarPara(Aluno indicado) {
+        // logica de envio de emails com a lib Java Mail
+    }
+}
