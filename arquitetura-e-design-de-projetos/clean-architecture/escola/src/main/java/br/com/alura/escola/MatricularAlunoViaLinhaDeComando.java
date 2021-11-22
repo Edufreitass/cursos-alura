c392128990f0252f690131cf0060afd0246a13a0
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-11-22 20:38:05 -0300

    Use cases

---
+++ arquitetura-e-design-de-projetos/clean-architecture/escola/src/main/java/br/com/alura/escola/MatricularAlunoViaLinhaDeComando.java
@@ -0,0 +1,17 @@
+package br.com.alura.escola;
+
+import br.com.alura.escola.aplicacao.aluno.matricular.MatricularAluno;
+import br.com.alura.escola.aplicacao.aluno.matricular.MatricularAlunoDTO;
+import br.com.alura.escola.infraestrutura.aluno.RepositorioDeAlunosEmMemoria;
+
+public class MatricularAlunoViaLinhaDeComando {
+
+    public static void main(String[] args) {
+        String nome = "Fulano da Silva";
+        String cpf = "123.456.789-00";
+        String email = "fulano@email.com";
+
+        MatricularAluno matricular = new MatricularAluno(new RepositorioDeAlunosEmMemoria());
+        matricular.executa(new MatricularAlunoDTO(nome, cpf, email));
+    }
+}
