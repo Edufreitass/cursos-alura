ee3dbca0073f3e7f0b03d1e9e61af07b1bdc70b3
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-11-25 19:54:27 -0300

    Separando os contextos

---
+++ arquitetura-e-design-de-projetos/domain-driven-design/escola/src/main/java/br/com/alura/escola/academico/dominio/aluno/LogDeAlunoMatriculado.java
@@ -0,0 +1,20 @@
+package br.com.alura.escola.academico.dominio.aluno;
+
+import br.com.alura.escola.academico.dominio.Ouvinte;
+import br.com.alura.escola.academico.dominio.Evento;
+
+import java.time.format.DateTimeFormatter;
+
+public class LogDeAlunoMatriculado extends Ouvinte {
+
+    @Override
+    public void reageAo(Evento evento) {
+        String momentoFormatado = evento.momento().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
+        System.out.println(String.format("Aluno com CPF %s matriculado em: %s", ((AlunoMatriculado) evento).getCpfDoAluno(), momentoFormatado));
+    }
+
+    @Override
+    protected boolean deveProcessar(Evento evento) {
+        return evento instanceof AlunoMatriculado;
+    }
+}
