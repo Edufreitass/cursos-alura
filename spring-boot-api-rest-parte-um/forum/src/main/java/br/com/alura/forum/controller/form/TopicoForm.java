4d8fa2be653c8cf00cb33ec3b829dc493db85fc3
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-11-27 12:02:40 -0300

    Validações com Bean Validation

--- spring-boot-api-rest-parte-um/forum/src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
+++ spring-boot-api-rest-parte-um/forum/src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -4,12 +4,26 @@ import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Getter
 public class TopicoForm {
 
+    @NotNull
+    @NotEmpty
+    @Length(min = 5)
     private String titulo;
+
+    @NotNull
+    @NotEmpty
+    @Length(min = 10)
     private String mensagem;
+
+    @NotNull
+    @NotEmpty
     private String nomeCurso;
 
     public Topico converter(CursoRepository cursoRepository) {
