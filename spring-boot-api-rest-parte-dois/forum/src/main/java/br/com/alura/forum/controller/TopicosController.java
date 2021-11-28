c92a1cf02f19c9f12351aa5c046efd70a8c5f61d
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-11-28 12:33:51 -0300

    Simplificando a paginação e ordenação

--- spring-boot-api-rest-parte-dois/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ spring-boot-api-rest-parte-dois/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -9,9 +9,9 @@ import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -33,11 +33,12 @@ public class TopicosController {
 
     @GetMapping
     public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso,
-                                 @RequestParam int pagina,
-                                 @RequestParam int qtd,
-                                 @RequestParam String ordenacao) {
-        Pageable paginacao = PageRequest.of(pagina, qtd, Sort.Direction.DESC, ordenacao);
-
+                                 @PageableDefault(
+                                         sort = "id",
+                                         direction = Sort.Direction.DESC,
+                                         page = 0,
+                                         size = 10)
+                                         Pageable paginacao) {
         Page<Topico> topicos;
         if (nomeCurso == null) {
             topicos = topicoRepository.findAll(paginacao);
