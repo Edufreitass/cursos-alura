90f55af11d99b897b2d667e8877012e62d54077d
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-11-28 14:11:02 -0300

    Autenticando o usuário

--- spring-boot-api-rest-parte-dois/forum/src/main/resources/data.sql
+++ spring-boot-api-rest-parte-dois/forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$wywIblzy41hd5qrguiZomuVIQyoOLCD8aO5sm0GIw9c.3FZjznicy');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
