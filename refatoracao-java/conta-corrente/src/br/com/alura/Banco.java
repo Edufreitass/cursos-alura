8aeae3019168ad5e381abaa4579d42683f2f56f5
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-12-01 22:08:48 -0300

    Pequeno exemplo de refatoração

--- refatoracao-java/conta-corrente/src/br/com/alura/Banco.java
+++ refatoracao-java/conta-corrente/src/br/com/alura/Banco.java
@@ -3,11 +3,11 @@ package br.com.alura;
 public class Banco {
 
     public void depositar(Conta c, double valor) {
-        c.setSaldo(c.getSaldo() + valor);
+        c.sacar(valor);
     }
 
     public void realizarTransferencia(Conta a, double valor, Conta c) {
-        a.setSaldo(a.getSaldo() - valor);
-        c.setSaldo(c.getSaldo() + valor);
+        a.sacar(valor);
+        c.depositar(valor);
     }
 }
--- refatoracao-java/conta-corrente/src/br/com/alura/Conta.java
+++ refatoracao-java/conta-corrente/src/br/com/alura/Conta.java
@@ -8,6 +8,14 @@ public class Conta {
         this.saldo = saldo;
     }
 
+    public void sacar(double valor) {
+        this.saldo = this.saldo - valor;
+    }
+
+    public void depositar(double valor) {
+        this.saldo = this.saldo + valor;
+    }
+
     public double getSaldo() {
         return saldo;
     }
