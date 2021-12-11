b6fbea6ef1955c45f7b9f7fd11e3c748268435a8
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-12-10 21:24:52 -0300

    Assumindo o controle

--- do-jdbc-ao-spring-data/java-e-jdbc-trabalhando-com-um-banco-de-dados/loja-virtual-repository/src/br/com/alura/TestaInsercaoComParametro.java
+++ do-jdbc-ao-spring-data/java-e-jdbc-trabalhando-com-um-banco-de-dados/loja-virtual-repository/src/br/com/alura/TestaInsercaoComParametro.java
@@ -7,14 +7,23 @@ public class TestaInsercaoComParametro {
     public static void main(String[] args) throws SQLException {
         ConnectionFactory connectionFactory = new ConnectionFactory();
         Connection connection = connectionFactory.recuperarConexao();
+        connection.setAutoCommit(false);
 
-        String nome = "Mouse";
-        String descricao = "Mouse sem fio";
         String sql = "INSERT INTO PRODUTO(nome, descricao) VALUES(?, ?)";
 
         PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        adicionarVariavel("SmartTV", "45 polegadas", preparedStatement);
+        adicionarVariavel("Radio", "Radio de bateria", preparedStatement);
+    }
+
+    private static void adicionarVariavel(String nome, String descricao, PreparedStatement preparedStatement) throws SQLException {
         preparedStatement.setString(1, nome);
         preparedStatement.setString(2, descricao);
+
+//        if (nome.equals("Radio")) {
+//            throw new RuntimeException("Não foi possível adicionar o produto");
+//        }
+
         preparedStatement.execute();
 
         ResultSet resultSet = preparedStatement.getGeneratedKeys();
