24b5ec310d2ae176fda0c740f49709d5af455037
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-12-12 19:52:30 -0300

    Implementando as controllers

--- do-jdbc-ao-spring-data/java-e-jdbc-trabalhando-com-um-banco-de-dados/loja-virtual-view-repository/src/br/com/alura/jdbc/controller/CategoriaController.java
+++ do-jdbc-ao-spring-data/java-e-jdbc-trabalhando-com-um-banco-de-dados/loja-virtual-view-repository/src/br/com/alura/jdbc/controller/CategoriaController.java
@@ -1,15 +1,22 @@
 package br.com.alura.jdbc.controller;
 
+import br.com.alura.jdbc.dao.CategoriaDAO;
+import br.com.alura.jdbc.factory.ConnectionFactory;
 import br.com.alura.jdbc.modelo.Categoria;
 
-import java.util.ArrayList;
+import java.sql.Connection;
 import java.util.List;
 
 public class CategoriaController {
 
+    private final CategoriaDAO categoriaDAO;
+
+    public CategoriaController() {
+        Connection connection = new ConnectionFactory().recuperarConexao();
+        this.categoriaDAO = new CategoriaDAO(connection);
+    }
+
     public List<Categoria> listar() {
-        List<Categoria> categorias = new ArrayList<>();
-        categorias.add(new Categoria(1, "Categoria de teste"));
-        return categorias;
+        return this.categoriaDAO.listar();
     }
 }
