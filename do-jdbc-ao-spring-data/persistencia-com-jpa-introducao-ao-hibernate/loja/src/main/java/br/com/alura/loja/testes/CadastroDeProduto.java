5508c083c6752acf31dbf7baac643dc11c680812
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-12-12 23:01:45 -0300

    Mapeando relacionamentos

--- do-jdbc-ao-spring-data/persistencia-com-jpa-introducao-ao-hibernate/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ do-jdbc-ao-spring-data/persistencia-com-jpa-introducao-ao-hibernate/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja.testes;
 
+import br.com.alura.loja.dao.CategoriaDAO;
 import br.com.alura.loja.dao.ProdutoDAO;
 import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
@@ -11,13 +12,18 @@ import java.math.BigDecimal;
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
-        Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), Categoria.CELULARES);
+        Categoria celulares = new Categoria("CELULARES");
+        Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
 
         EntityManager em = JPAUtil.getEntityManager();
-        ProdutoDAO dao = new ProdutoDAO(em);
+        CategoriaDAO categoriaDAO = new CategoriaDAO(em);
+        ProdutoDAO produtoDAO = new ProdutoDAO(em);
 
         em.getTransaction().begin();
-        dao.cadastrar(celular);
+
+        categoriaDAO.cadastrar(celulares);
+        produtoDAO.cadastrar(celular);
+
         em.getTransaction().commit();
         em.close();
     }
