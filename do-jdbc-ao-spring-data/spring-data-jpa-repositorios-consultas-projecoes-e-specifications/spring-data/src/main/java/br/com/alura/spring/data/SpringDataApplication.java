22b73e76c6a48359a1ee47dd317ffc054c990c12
Eduardo Freitas <eduardoflorencio96@gmail.com>
2021-12-15 23:25:55 -0300

    Salvando o registro

--- do-jdbc-ao-spring-data/spring-data-jpa-repositorios-consultas-projecoes-e-specifications/spring-data/src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ do-jdbc-ao-spring-data/spring-data-jpa-repositorios-consultas-projecoes-e-specifications/spring-data/src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -1,18 +1,20 @@
 package br.com.alura.spring.data;
 
-import br.com.alura.spring.data.orm.Cargo;
-import br.com.alura.spring.data.repository.CargoRepository;
+import br.com.alura.spring.data.service.CargoService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class SpringDataApplication implements CommandLineRunner {
 
-    private final CargoRepository cargoRepository;
+    private final CargoService cargoService;
+    private Boolean system = true;
 
-    public SpringDataApplication(CargoRepository cargoRepository) {
-        this.cargoRepository = cargoRepository;
+    public SpringDataApplication(CargoService cargoService) {
+        this.cargoService = cargoService;
     }
 
 	public static void main(String[] args) {
@@ -20,10 +22,22 @@ public class SpringDataApplication implements CommandLineRunner {
 	}
 
     @Override
-    public void run(String... args) throws Exception {
-        Cargo cargo = new Cargo();
-        cargo.setDescricao("DESENVOLVEDOR DE SOFTWARE");
+    public void run(String... args) {
+        Scanner scanner = new Scanner(System.in);
 
-        cargoRepository.save(cargo);
+        while (system) {
+            System.out.println("Qual acao voce deseja executar?");
+            System.out.println("0 - Sair");
+            System.out.println("1 - Cargo");
+
+            int action = scanner.nextInt();
+            if (action == 1) {
+                cargoService.inicial(scanner);
+            } else {
+                system = false;
+            }
+        }
+
+        scanner.close();
     }
 }
---
+++ do-jdbc-ao-spring-data/spring-data-jpa-repositorios-consultas-projecoes-e-specifications/spring-data/src/main/java/br/com/alura/spring/data/service/CargoService.java
@@ -0,0 +1,30 @@
+package br.com.alura.spring.data.service;
+
+import br.com.alura.spring.data.orm.Cargo;
+import br.com.alura.spring.data.repository.CargoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Scanner;
+
+@Service
+public class CargoService {
+
+    private final CargoRepository cargoRepository;
+
+    public CargoService(CargoRepository cargoRepository) {
+        this.cargoRepository = cargoRepository;
+    }
+
+    public void inicial(Scanner scanner) {
+        salvar(scanner);
+    }
+
+    public void salvar(Scanner scanner) {
+        System.out.println("Descricao do cargo:");
+        String descricao = scanner.next();
+        Cargo cargo = new Cargo();
+        cargo.setDescricao(descricao);
+        cargoRepository.save(cargo);
+        System.out.println("Salvo");
+    }
+}
